45c2981d0a185407900bd1b3bb033a78eafa7fa9
namangirdhar16 <64246118+namangirdhar16@users.noreply.github.com>
2020-12-29 19:35:33 +0530

    application connected to mongoDB cluster

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { reset } = require("nodemon");
 const  mongoose = require("mongoose");
+const objectID = mongoose.Types.ObjectId;
+
+
 
 const lodash = require("lodash") ; 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -22,11 +25,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1/blogDB",{useNewUrlParser:true , useUnifiedTopology:true }) ;
+mongoose.connect("mongodb+srv://naman1611:123$@cluster0.vjofi.mongodb.net/blogDB?retryWrites=true&w=majority/blogDB",{useNewUrlParser:true , useUnifiedTopology:true }) ;
 
 const postSchema = {
   postTitle :String ,
   postBody : String , 
+  postValidation: String,
 };
 
 const Post = mongoose.model("Post",postSchema);
@@ -77,12 +81,9 @@ app.post('/compose',(req,res)=>{
    const post = new Post({
       postTitle : req.body.postTitle , 
       postBody : req.body.postBody , 
+      postValidation : lodash.lowerCase(req.body.postTitle) ,
    }) ; 
-   post.save((err)=>{
-     if(err)
-     res.redirect("/");
-   }) ;
-   
+    post.save();
    res.redirect("/");
   
    
@@ -102,23 +103,98 @@ app.post('/posts/',(req,res)=>{
 
 
 })
-app.get("/posts/:postId", function(req, res){
+app.get("/posts/:postId", (req,res)=>{
 
 const requestedPostId = req.params.postId;
+  if(objectID.isValid(requestedPostId))
+  {
 
-  Post.findOne({_id: requestedPostId}, function(err, post){
-    res.render("post", {post:{
-      postTitle: post.postTitle,
-      postBody: post.postBody
-    }});
+    Post.findOne({_id: requestedPostId}, (err,post)=>{
+    if(err)
+    { console.log('error');
+      return res.redirect("/");
+    }
+    else
+    // res.render("post", {post:{
+    //   postTitle: post.postTitle,
+    //   postBody: post.postBody
+    // }});
+    {
+      console.log(post);
+      res.render("post",{post:
+      post});
+    }
   });
+  }
+  else{
+    requestedPostSearch = lodash.lowerCase(requestedPostId);
+    Post.findOne({postValidation:requestedPostSearch},(err,post)=>{
+        if(err||post==null)
+      return res.render("post",{post:{
+        postTitle:'Invalid Search',
+        postBody:'Oops, No Such post exists!',
+      }})
+      else
+      res.render("post",{post:post});
+      // console.log(post);
+      // res.redirect("/");
+    })
+
+  }
+
+ 
+  
+  
 
 });
 
 
+app.get("/delete",(req,res)=>{
+  res.render("delete");
+})
 
-
-
+app.post("/delete",(req,res)=>{
+   const deleteTitle = lodash.lowerCase(req.body.postTitle);
+   console.log(deleteTitle);
+   if(deleteTitle==="home page")
+   {
+     return res.render("post",{post:{
+       postTitle:`Sorry you don't have the access to delete home page content`,
+       postBody: 'Delete Some Other post',
+     }})
+   }
+   else if(objectID.isValid(deleteTitle))
+   {
+     Post.findOne({_id:deleteTitle},(err,post)=>{
+       if(err||post==null)
+       return res.render("post",{post:{
+         postTitle:'Invalid post',
+         postBody:'No such post exits!',
+       }})
+       res.redirect("/delete/"+post.postValidation) ;
+     })
+   }
+   else{
+     
+     Post.findOne({postValidation:deleteTitle},(err,post)=>{
+       
+       if(err||post==null)
+       return res.render("post",{post:{
+         postTitle:'Invalid post',
+         postBody:'No such post exits!',
+       }})
+       res.redirect("/delete/"+post.postValidation) ;
+     })
+   }
+})
+app.get("/delete/:searchTitle",(req,res)=>{
+   
+   Post.deleteOne({postValidation:req.params.searchTitle}).then(()=>{
+     console.log("post got deleted!");
+     
+   }).catch((err)=>{console.log('error')})
+    res.redirect("/");
+})
 
 
 
